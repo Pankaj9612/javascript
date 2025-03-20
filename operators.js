f1a17daa94b2349596a95559ef81c7edcf6d3058
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-03-20 22:37:13 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,30 @@
+console.log("Operatores in js");
+console.log("Arithmetic Operatores in js");
+let a=45;
+let b=4;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a++);
+console.log(a--);
+console.log(--a);
+console.log(--b);
+console.log(++b);
+console.log(b++);
+
+console.log("Assignment Operatores in js");
+c=10;
+console.log(c+=10);
+console.log(c*=45);
+console.log(c/=5);
+
+console.log("Comparision Operatores in js");
+let comp1=10;
+let comp2=20;
+console.log(comp1==comp2);
+console.log(comp1===comp2);
+console.log(comp1!==comp2);
+console.log(comp1>comp2);
+console.log(comp1<comp2);
