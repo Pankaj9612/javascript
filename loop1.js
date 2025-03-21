ed7426361f9480670f7e825adf7f7106f015f8c3
Pankaj Gaikwad <163497297+Pankaj9612@users.noreply.github.com>
2025-03-21 15:08:26 +0530

    Add files via upload

---
+++ loop1.js
@@ -0,0 +1,5 @@
+// simple for loop program..
+for (let i = 1; i <= 10; i++)
+{
+	console.log(i);
+	}
---
+++ loop2.js
@@ -0,0 +1,8 @@
+// sum of 10 natural number...
+let sum=0;
+let n=10;
+for(let i=0; i<=n; i++)
+{
+	sum= sum+i;
+}
+console.log(sum);
